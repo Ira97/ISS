95bd531b4c3798c504a84b2f7da7efa75dc55621
Сережа <ira1996199609@mail.ru>
2021-05-19 23:07:27 +1000

    Испр

--- VegaCore/VegaCore/Controllers/AreaController.cs
+++ VegaCore/VegaCore/Controllers/AreaController.cs
@@ -21,7 +21,7 @@ namespace Vega.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var areaList = await _areaProvider.GetAreaListAsync();
+            var model = await _areaProvider.GetAreaListAsync();
             //var model = new AreaViewModel()
             //{
             //    AreaList = new List<AreaDto>
